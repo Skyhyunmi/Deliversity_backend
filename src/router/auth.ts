27d3d514991ad20f8bcb655b97a8d56bf5ab1ead
psb9663 <psb9663@gmail.com>
2020-10-09 16:40:21 +0900

    lint error fixed

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -17,11 +17,11 @@ dotenv.config();
 
 const email_veriRep = db.getRepository(Email_Verify);
 
-function makeSignature(urlsub:string,timestamp:string){
+function makeSignature(urlsub: string, timestamp: string) {
   const space = " ";
   const newLine = "\n";
   const method = "POST";
-  const hmac=crypto.createHmac('sha256',process.env.NAVER_SECRET as string);
+  const hmac = crypto.createHmac('sha256', process.env.NAVER_SECRET as string);
   const mes = [];
   mes.push(method);
   mes.push(space);
@@ -61,7 +61,7 @@ auth.post("/login", function (req: any, res: Response, next: NextFunction) {
     user: any,
     info: any
   ) {
-    if (info==={})
+    if (info === {})
       return res.status(403).json(util.successFalse(null, info.message, null));
     if (err || !user) {
       return res
@@ -88,20 +88,20 @@ auth.post("/sms",/*util.isLoggedin,*/async function (req: any, res: Response, ne
   const body = req.body;
   const phone = body.phone;
   const sendFrom = process.env.SEND_FROM;
-  const serviceID=urlencode.encode(process.env.NAVER_SMS_SERVICE_ID as string);
+  const serviceID = urlencode.encode(process.env.NAVER_SMS_SERVICE_ID as string);
   const timestamp = Date.now().toString();
   const urlsub = `/sms/v2/services/${serviceID}/messages`;
-  const signature=makeSignature(urlsub,timestamp);
+  const signature = makeSignature(urlsub, timestamp);
   const randomNumber = Math.floor(Math.random() * (999999 - 100000)) + 100000;
-  const data={
-    "type":"SMS",
-    "contentType":"COMM",
-    "countryCode":"82",
-    "from":sendFrom,
-    "content":`Deliversity 인증번호 ${randomNumber} 입니다.`,
-    "messages":[
+  const data = {
+    "type": "SMS",
+    "contentType": "COMM",
+    "countryCode": "82",
+    "from": sendFrom,
+    "content": `Deliversity 인증번호 ${randomNumber} 입니다.`,
+    "messages": [
       {
-        "to":phone
+        "to": phone
       }
     ]
   };
@@ -114,7 +114,7 @@ auth.post("/sms",/*util.isLoggedin,*/async function (req: any, res: Response, ne
     const getToken = await axios({
       url: `https://sens.apigw.ntruss.com/sms/v2/services/${serviceID}/messages`,
       method: "post", // POST method
-      headers: { 
+      headers: {
         "Content-Type": "application/json; charset=utf-8",
         "x-ncp-apigw-timestamp": timestamp,
         "x-ncp-iam-access-key": process.env.NAVER_KEY,
@@ -125,15 +125,15 @@ auth.post("/sms",/*util.isLoggedin,*/async function (req: any, res: Response, ne
     // console.log(getToken);
     const tokenData = getToken.data;
     veriRep.create({
-      phone:phone,
-      sendId:tokenData.requestId,
-      number:randomNumber
+      phone: phone,
+      sendId: tokenData.requestId,
+      number: randomNumber
     });
-    if(tokenData.statusCode == "202")
+    if (tokenData.statusCode == "202")
       return res.json(util.successTrue(tokenData.statusName));
     else return res.status(403).json(util.successFalse(null, tokenData.statusName, null));
   }
-  catch(e){
+  catch (e) {
     console.error(e);
     veriRep.destroy({
       where: {
@@ -143,25 +143,25 @@ auth.post("/sms",/*util.isLoggedin,*/async function (req: any, res: Response, ne
   }
 });
 
-auth.post("/sms/verification",async function (req: any, res: Response, next: NextFunction) {
-  const body =req.body;
-  const verify=body.verify;
-  const phone=body.phone;
+auth.post("/sms/verification", async function (req: any, res: Response, next: NextFunction) {
+  const body = req.body;
+  const verify = body.verify;
+  const phone = body.phone;
   try {
     veriRep.findOne({
       where: {
         phone: phone
       }
     }).then(function (veri) {
-      if(veri){
-        if(veri.number == verify){
+      if (veri) {
+        if (veri.number == verify) {
           const now = Number.parseInt(Date.now().toString());
           const created = Date.parse(veri.createdAt);
-          const remainingTime = (now-created)/60000;
-          if(remainingTime>3){ //3분
+          const remainingTime = (now - created) / 60000;
+          if (remainingTime > 3) { //3분
             return res.status(403).json(util.successFalse(null, "time expired.", null));
           }
-          veriRep.update({verified:true},{where:{phone:phone}});
+          veriRep.update({ verified: true }, { where: { phone: phone } });
           return res.json(util.successTrue("matched."));
         }
         else return res.status(403).json(util.successFalse(null, "not matched.", null));
@@ -170,7 +170,7 @@ auth.post("/sms/verification",async function (req: any, res: Response, next: Nex
         return res.status(403).json(util.successFalse(null, "not matched.", null));
       }
     });
-  } catch(e){
+  } catch (e) {
     console.error(e);
   }
 });
@@ -179,16 +179,16 @@ auth.get('/google', passport.authenticate('google', {
   scope: ["profile", "email"]
 }));
 
-auth.get('/google/callback', function(req: any, res: Response, next: NextFunction) {
-  passport.authenticate('google',function (
+auth.get('/google/callback', function (req: any, res: Response, next: NextFunction) {
+  passport.authenticate('google', function (
     err: any,
     user: any,
     info: any
-  ){
-    if (info){
-      if(info.message)
+  ) {
+    if (info) {
+      if (info.message)
         return res.status(403).json(util.successFalse(null, info.message, info.auth));
-        //회원가입 redirect -> auth 값은 googleOAuth에 넣는다.(프론트에서)
+      //회원가입 redirect -> auth 값은 googleOAuth에 넣는다.(프론트에서)
     }
     if (err || !user) {
       return res
@@ -208,7 +208,7 @@ auth.get('/google/callback', function(req: any, res: Response, next: NextFunctio
       });
       res.json({ token: user.authToken, admin: user.admin });
     });
-  })(req,res,next);
+  })(req, res, next);
 }
 );
 
@@ -218,8 +218,8 @@ auth.get('/kakao/callback', function (req, res, next) {
   passport.authenticate('kakao', function (err, user) {
     console.log('passport.authenticate(kakao)실행');
     // if (!user) { return res.redirect('http://localhost:3000/login'); }
-    req.logIn(user, function (err) { 
-       
+    req.logIn(user, function (err) {
+
       if (err) return res.status(403).json(util.successFalse(err, "", null));
       const payload = {
         id: user.userId,
@@ -230,21 +230,21 @@ auth.get('/kakao/callback', function (req, res, next) {
       user.authToken = jwt.sign(payload, process.env.JWT_SECRET as jwt.Secret, {
         expiresIn: '7d',
       });
-      res.json({ token: user.authToken, admin: user.admin });      
+      res.json({ token: user.authToken, admin: user.admin });
     });
   })(req, res);
 });
 auth.post("/email",/*util.isLoggedin,*/async function (req: any, res: Response, next: NextFunction) {
-    const body = req.body;
-    const email = body.email;
+  const body = req.body;
+  const email = body.email;
 
-    var key_one = crypto.randomBytes(256).toString('hex').substr(100,5);
-    var key_two = crypto.randomBytes(256).toString('base64').substr(50,5);
-    var email_number = key_one + key_two;
+  const key_one = crypto.randomBytes(256).toString('hex').substr(100, 5);
+  const key_two = crypto.randomBytes(256).toString('base64').substr(50, 5);
+  const email_number = key_one + key_two;
 
-    // Use SMTP transport
-    var transporter = nodemailer.createTransport(
-      {
+  // Use SMTP transport
+  const transporter = nodemailer.createTransport(
+    {
       service: 'Gmail',
       host: 'smtp.gmail.com',
       port: 587,
@@ -254,71 +254,72 @@ auth.post("/email",/*util.isLoggedin,*/async function (req: any, res: Response, 
         pass: process.env.MAIL_PW
       }
     }
-    );
+  );
 
-     try {
-       email_veriRep.destroy({
-         where: {
-           email : email
-         }
-       });
-      
-    var url = 'http://'+req.get('host')+'/api/v1/auth/email/verification'+'?email_number='+email_number;
-    var info = await transporter.sendMail({
-      from : '"Deliversity" <${process.env.MAIL_ID}>',
+  try {
+    email_veriRep.destroy({
+      where: {
+        email: email
+      }
+    });
+
+    const url = 'http://' + req.get('host') + '/api/v1/auth/email/verification' + '?email_number=' + email_number;
+    const info = await transporter.sendMail({
+      from: '"Deliversity" <${process.env.MAIL_ID}>',
       to: email,
-      subject:"Deliversity 인증 메일입니다.",
-      html: "<h3>이메일 인증을 위해 URL을 클릭해주세요.</h3><br>"+url
+      subject: "Deliversity 인증 메일입니다.",
+      html: "<h3>이메일 인증을 위해 URL을 클릭해주세요.</h3><br>" + url
     });
 
     email_veriRep.create({
-      email:email,
-      email_number:email_number
+      email: email,
+      email_number: email_number
     });
 
     res.status(200).json({
-      status:'Success',
-      code:200,
-      message:'Sent Auth Email',
+      status: 'Success',
+      code: 200,
+      message: 'Sent Auth Email',
     });
   }
-  catch(e){
+  catch (e) {
     console.error(e);
     email_veriRep.destroy({
       where: {
-        email : email
+        email: email
       }
     });
   }
 }
 );
 
-auth.get('/email/verification',async (req, res, next: NextFunction) => {  
+auth.get('/email/verification', async (req, res, next: NextFunction) => {
   const email_number = req.query.email_number as string;
   console.log("1");
   email_veriRep.findOne({
-    where:{email_number:email_number}
+    where: { email_number: email_number }
   }).then((email_veri) => {
-      if (email_veri) {
-        console.log("2");
-        email_veriRep.update({
-          email_verified: true
-        }, {
-          where: { email:email_veri.email }
-        });
-        res.status(204).json({
-          status: 'Success',
-          code: 204,
-          message: 'Matched',
-        });
-      }
-      else { console.log("3");
-        res.status(403).json({
-          status: 'Fail',
-          code: 403,
-          message: 'Not Matched',
-        });
-      }
+    if (email_veri) {
+      console.log("2");
+      email_veriRep.update({
+        email_verified: true
+      }, {
+        where: { email: email_veri.email }
+      });
+      res.status(204).json({
+        status: 'Success',
+        code: 204,
+        message: 'Matched',
+      });
     }
+    else {
+      console.log("3");
+      res.status(403).json({
+        status: 'Fail',
+        code: 403,
+        message: 'Not Matched',
+      });
+    }
+  }
   );
 });
