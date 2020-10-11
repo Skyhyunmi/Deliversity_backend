d4afd7081ff009689dbea6d5eb9152a466da2130
skyhyunmi <codikh1@gmail.com>
2020-10-12 00:57:12 +0900

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,17 @@
 REPOSITORY=/home/ubuntu/app/step2
 PROJECT_NAME=pm2
 
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/* $REPOSITORY/
+
+cd $REPOSITORY/
+
 npm prune
 
 npm i
 
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/zip/* $REPOSITORY/
+npm run build
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -17,13 +21,11 @@ CURRENT_PID=$(pgrep -fl $PROJECT_NAME | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
+
+
 if [ -z "$CURRENT_PID" ]; then
    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-   echo "> kill -15 $CURRENT_PID"
-   kill -15 $CURRENT_PID
-   sleep 5
-   npm run start
+   pm2 start dist/app.js
 fi
 
 echo "> 새 어플리케이션 배포"
