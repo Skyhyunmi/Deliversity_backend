17ecc450773f77ff678e61eafe884f3fd28d5056
skyhyunmi <codikh1@gmail.com>
2020-10-27 14:38:00 +0900

    본인 정보 반환, 수정 함수

--- src/router/myinfo.ts
+++ src/router/myinfo.ts
@@ -1,10 +1,92 @@
 import { NextFunction, Response, Router } from "express";
 import * as util from "../config/util";
 import { userRep, addressRep } from "../models/index";
+import * as crypto from "crypto";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const myinfo = Router();
+
+myinfo.get('/', util.isLoggedin, async function (req: any, res: Response, next: NextFunction) {
+//본인 정보 반환
+  const tokenData = req.decoded;
+  try{
+    const _user = await userRep.findOne({
+      where:{
+        id:tokenData.id
+      }
+    });
+    if(!_user) return res.status(403).json(util.successFalse(null,"해당 하는 유저가 없습니다.",null));
+    const user = {
+      id:_user.id,
+      userId:_user.userId,
+      name:_user.name,
+      nickName:_user.nickName,
+      gender:_user.gender,
+      age:_user.age,
+      email:_user.email,
+      phone:_user.phone,
+      addressId:_user.addressId,
+      grade:_user.grade,
+      createdAt:_user.createdAt,
+      updatedAt:_user.updatedAt
+    };
+    return res.json(util.successTrue("",user));
+  }catch(err){
+    return res.status(403).json(util.successFalse(err,"",null));
+  }
+});
+
+myinfo.put('/', util.isLoggedin, async function (req: any, res: Response, next: NextFunction) {
+//본인 정보 수정
+  const tokenData = req.decoded;
+  const reqBody = req.body;
+  let salt=null,hashedPw=null;
+  try{
+    const _user = await userRep.findOne({
+      where:{
+        id:tokenData.id
+      }
+    });
+    if(!_user) return res.status(403).json(util.successFalse(null,"해당 하는 유저가 없습니다.",null));
+    if(reqBody.pw){
+      const buffer = crypto.randomBytes(64);
+      salt = buffer.toString('base64');
+      const key = crypto.pbkdf2Sync(reqBody.pw, salt, 100000, 64, 'sha512');
+      hashedPw = key.toString('base64');
+    }
+    if(reqBody.nickName){
+      const nickExist = await userRep.findOne({
+        where: {
+          nickName: reqBody.nickName
+        }
+      });
+      if(nickExist) return res.status(403).json(util.successFalse(null,"nickName duplicated.",null));
+    }
+    _user.update({
+      password:hashedPw?hashedPw:_user.password,
+      salt:salt?salt:_user.salt,
+      nickName:reqBody.nickName?reqBody.nickName:_user.nickName
+    });
+    const user = {
+      id:_user.id,
+      userId:_user.userId,
+      name:_user.name,
+      nickName:_user.nickName,
+      gender:_user.gender,
+      age:_user.age,
+      email:_user.email,
+      phone:_user.phone,
+      addressId:_user.addressId,
+      grade:_user.grade,
+      createdAt:_user.createdAt,
+      updatedAt:_user.updatedAt
+    };
+    return res.json(util.successTrue("",user));
+  }catch(err){
+    return res.status(403).json(util.successFalse(err,"",null));
+  }
+});
 
 myinfo.get('/address/list', util.isLoggedin, async function (req: any, res: Response, next: NextFunction) {
 //자기 주소 리스트 반환
@@ -135,11 +217,14 @@ myinfo.delete('/address', util.isLoggedin, async function (req: any, res: Respon
   const tokenData = req.decoded;
   const reqBody = req.body;
   try{
-    addressRep.destroy({
+    const address = await addressRep.findOne({
       where:{
         id: reqBody.addressId
       }
-    }).then(()=> res.json(util.successTrue("Deletion Success.",null)));
+    });
+    if(!address) return res.json(util.successFalse(null,"Deletion Failure.",null));
+    address.destroy().then(()=>res.json(util.successTrue("Deletion Success.",null)));
+
   }catch(err){
     return res.status(403).json(util.successFalse(err,"",null));
   }
