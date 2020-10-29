2d16135be9ee333189a376b5b073f32705a1714c
Park SuBin <psb9663@ajou.ac.kr>
2020-10-29 13:06:16 +0900

    add admin

--- src/models/report.ts
+++ src/models/report.ts
@@ -13,57 +13,57 @@ import {
   DeletedAt,
   Default//, ForeignKey
 } from "sequelize-typescript";
-  
-  @Table({ timestamps: true })
+
+// 이거 테스트용으로 임시로 작성해서 false로 꺼놈
+@Table({ timestamps: false })
 export default class Report extends Model<Report> {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.BIGINT)
-    id!: number;
-  
-    @AllowNull(false)
-    @Column(DataType.STRING)
-    reportKind!: string;
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.BIGINT)
+  id!: number;
 
-    @AllowNull(false)
-    @Column(DataType.BIGINT)
-    orderId!: number;
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  reportKind!: string;
 
-    @AllowNull(false)
-    @Column(DataType.BIGINT)
-    userId!: number;
+  @AllowNull(false)
+  @Column(DataType.BIGINT)
+  orderId!: number;
 
-    @AllowNull(false)
-    @Column(DataType.BIGINT)
-    riderId!: number;
+  @AllowNull(false)
+  @Column(DataType.BIGINT)
+  userId!: number;
 
-    @AllowNull(false)
-    @Column(DataType.BIGINT)
-    fromId!: number;
-  
-    // @AllowNull(true)
-    @Column(DataType.TEXT)
-    chat!:string;
-  
-    // @AllowNull(true)
-    @Column(DataType.TEXT)
-    content!:string;
-    
-    @AllowNull(true)
-    @Column(DataType.TEXT)
-    answer!:string;
+  @AllowNull(false)
+  @Column(DataType.BIGINT)
+  riderId!: number;
 
-    @Default(false)
-    @Column(DataType.BOOLEAN)
-    stauts!:boolean;
+  @AllowNull(false)
+  @Column(DataType.BIGINT)
+  fromId!: number;
 
-    @CreatedAt
-    createdAt!: Date;
-  
-    @UpdatedAt
-    updatedAt!: Date;
-  
-    @DeletedAt
-    deletedAt!: Date;
+  // @AllowNull(true)
+  @Column(DataType.TEXT)
+  chat!: string;
+
+  // @AllowNull(true)
+  @Column(DataType.TEXT)
+  content!: string;
+
+  @AllowNull(true)
+  @Column(DataType.TEXT)
+  answer!: string;
+
+  @Default(0)
+  @Column(DataType.BOOLEAN)
+  status!: boolean;
+
+  @CreatedAt
+  createdAt!: Date;
+
+  @UpdatedAt
+  updatedAt!: Date;
+
+  @DeletedAt
+  deletedAt!: Date;
 }
-  
--- src/router/admin.ts
+++ src/router/admin.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response, Router } from "express";
 import * as util from "../config/util";
-import { userRep } from "../models/index";
+import { qnaRep, reportRep, userRep } from "../models/index";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -32,7 +32,7 @@ admin.post('/upload', /*util.isLoggedin, util.isAdmin,*/ async function (req: an
   const reqBody = req.body;
   const id = reqBody.id;
   try {
-    let photo;
+    let detail_upload;
     await userRep.findOne({
       where: {
         id: id
@@ -47,10 +47,10 @@ admin.post('/upload', /*util.isLoggedin, util.isAdmin,*/ async function (req: an
       else if (user.grade == 0) {
         return res.status(403).json(util.successFalse(null, "인증을 요청하지 않은 유저입니다.", null));
       }
-      photo = "유저 아이디: " + user.id + "유저 닉네임: " + user.userId + "사진 주소: " + user.idCard;
+      detail_upload = "유저 아이디: " + user.id + "유저 닉네임: " + user.userId + "사진 주소: " + user.idCard;
     });
-    if (!photo) return res.status(403).json(util.successFalse(null, "해당 유저가 사진을 등록하지 않았습니다.", null));
-    return res.json(util.successTrue("", photo));
+    if (!detail_upload) return res.status(403).json(util.successFalse(null, "해당 유저가 사진을 등록하지 않았습니다.", null));
+    return res.json(util.successTrue("", detail_upload));
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
@@ -90,68 +90,146 @@ admin.put('/upload', /*util.isLoggedin, util.isAdmin,*/ async function (req: any
 });
 
 
-admin.get('/reports', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.get('/reports', /*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //신고 리스트 반환
-  const tokenData = req.decoded;
-  const reqBody = req.body;
   try {
-    //작성
+    let reportList = "";
+    await reportRep.findAll({
+      where: {
+        status: "0"
+      }
+    }).then((lists) => {
+      console.log("?");
+      for (let i = 0; i < lists.length; i++) {
+        reportList += i + 1 + ". " + "reportId: " + lists[i].id + "orderId: " + lists[i].orderId + ", reportKind: " + lists[i].reportKind + ", fromId: " + lists[i].fromId + "  ";
+      }
+    });
+    if (!reportList) return res.status(403).json(util.successFalse(null, "현재 처리를 기다리는 신고가 없습니다.", null));
+    return res.json(util.successTrue("", reportList));
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
 });
 
-admin.get('/report', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.post('/report',/*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //신고 상세내용보기
-  const tokenData = req.decoded;
   const reqBody = req.body;
+  const reportId = reqBody.reportId;
   try {
-    //작성
+    let detail_report;
+    await reportRep.findOne({
+      where: {
+        id: reportId
+      }
+    }).then((rp) => {
+      if (!rp) {
+        return res.status(403).json(util.successFalse(null, "해당하는 주문 내역이 없습니다.", null));
+      }
+      detail_report = "id: " + rp.id + ", reportKind: " + rp.reportKind + ", orderId: " + rp.orderId +
+        ", userId: " + rp.userId + ", riderId: " + rp.riderId + ", fromId: " + rp.fromId +
+        ", chat: " + "채팅 어쩌지" + ", content: " + rp.content;
+    });
+    return res.json(util.successTrue("", detail_report));
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
 });
 
-admin.put('/report', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.put('/report', /*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //신고 답변 작성
-  const tokenData = req.decoded;
   const reqBody = req.body;
+  const reportId = reqBody.reportId;
+  const answer = reqBody.answer;
   try {
-    //작성
+    await reportRep.findOne({
+      where: {
+        id: reportId
+      }
+    }).then((report) => {
+      if (!report) {
+        return res.status(403).json(util.successFalse(null, "해당하는 신고가 없습니다.", null));
+      }
+      if (report.status) {
+        return res.status(403).json(util.successFalse(null, "이미 처리된 문의입니다.", null));
+      }
+      report.update({
+        answer: answer,
+        status: true
+      });
+      return res.json(util.successTrue("", report));
+    });
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
 });
 
 
-admin.get('/qnas', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.get('/qnas', /*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //문의 리스트 반환
-  const tokenData = req.decoded;
-  const reqBody = req.body;
   try {
-    //작성
+    let qnaList = "";
+    await qnaRep.findAll({
+      where: {
+        status: "0"
+      }
+    }).then((lists) => {
+      for (let i = 0; i < lists.length; i++) {
+        qnaList += i + 1 + ". " + "qnaId: " + lists[i].id + ", qnaKind: " + lists[i].qnaKind + "  ";
+      }
+    });
+    if (!qnaList) return res.status(403).json(util.successFalse(null, "현재 처리를 기다리는 문의가 없습니다.", null));
+    return res.json(util.successTrue("", qnaList));
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
 });
 
-admin.get('/qna', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.post('/qna', /*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //문의 상세내용보기
-  const tokenData = req.decoded;
   const reqBody = req.body;
+  const qnaId = reqBody.qnaId;
   try {
-    //작성
+    let detail_qna;
+    await qnaRep.findOne({
+      where: {
+        id: qnaId
+      }
+    }).then((qna) => {
+      if (!qna) {
+        return res.status(403).json(util.successFalse(null, "해당하는 문의 내역이 없습니다.", null));
+      }
+      detail_qna = "id: " + qna.id + ", qnaKind: " + qna.qnaKind + ", userId: " + qna.userId +
+        ", content: " + qna.content;
+    });
+    return res.json(util.successTrue("", detail_qna));
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
 });
 
-admin.put('/qna', util.isLoggedin, util.isAdmin, async function (req: any, res: Response, next: NextFunction) {
+admin.put('/qna', /*util.isLoggedin, util.isAdmin,*/ async function (req: any, res: Response, next: NextFunction) {
   //문의 답변 작성
-  const tokenData = req.decoded;
   const reqBody = req.body;
+  const qnaId = reqBody.qnaId;
+  const answer = reqBody.answer;
   try {
-    //작성
+    await qnaRep.findOne({
+      where: {
+        id: qnaId
+      }
+    }).then((qna) => {
+      if (!qna) {
+        return res.status(403).json(util.successFalse(null, "해당하는 문의가 없습니다.", null));
+      }
+      if (qna.status) {
+        return res.status(403).json(util.successFalse(null, "이미 처리된 문의입니다.", null));
+      }
+      qna.update({
+        answer: answer,
+        status: true
+      });
+      return res.json(util.successTrue("", qna));
+    });
   } catch (err) {
     return res.status(403).json(util.successFalse(err, "", null));
   }
