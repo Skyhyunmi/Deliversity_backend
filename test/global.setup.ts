a0b4a90dcaf7d3c71ea8b355586a769ba287a3ac
psb9663 <psb9663@gmail.com>
2020-11-22 23:07:26 +0900

    change for db

--- test/global.setup.ts
+++ test/global.setup.ts
@@ -3,20 +3,25 @@ import { app } from "../src/app";
 import { myCache } from "../src/config/functions"
 
 import dotenv from "dotenv";
-dotenv.config({path:"../.env"});
+import { db } from "../src/models";
+dotenv.config({ path: "../.env" });
 
-async function signupFunc(email:string, id:string, nickName:string, phone:string){
+async function signupFunc(email: string, id: string, nickName: string, phone: string) {
     return await request(app)
-    .post('/api/v1/auth/signup')
-    .send({"age": "0", "email": email,
-        "gender": "male", "id": id, "name": "jesttest",
-        "nickName": nickName, "phone": phone, "pw": "jesttest",
-        "googleOAuth":process.env.TEST_GOOGLE_TOKEN,
-        "kakaoOAuth":process.env.TEST_KAKAO_TOKEN})
+        .post('/api/v1/auth/signup')
+        .send({
+            "age": "0", "email": email,
+            "id": id, "name": "jesttest",
+            "nickName": nickName, "phone": phone, "pw": "jesttest",
+            "googleOAuth": process.env.TEST_GOOGLE_TOKEN,
+            "kakaoOAuth": process.env.TEST_KAKAO_TOKEN
+        })
 }
 
 module.exports = async (globalConfig: any) => {
-    myCache.set("01000000000",{verify:1, updatedAt:Date.now()});
-    myCache.set("test@test.ac.kr",{verify:1, updatedAt:Date.now()});
-    await signupFunc("test@test.ac.kr","jesttest", "jesttest", "01000000000");
+    await db.sync({ force: true });
+    await db.query("INSERT INTO Users(userId, password, salt, name, nickName, gender, age, email, phone, addressId, grade, googleOAuth, firebaseUid, firebaseFCM,kakaoOAuth, idCard,lat,lng,createdAt,updatedAt) VALUES(" + process.env.query + ")");
+    myCache.set("01000000000", { verify: 1, updatedAt: Date.now() });
+    myCache.set("test@test.ac.kr", { verify: 1, updatedAt: Date.now() });
+    await signupFunc("test@test.ac.kr", "jesttest", "jesttest", "01000000000");
 };
